9c288accf7151eccd87e631c609cb43abc2a28c7
Jason <jason.ng@cibc-technology.com>
2024-10-11 17:51:56 +0800

    Add project files.

---
+++ globalblue1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using globalblue1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace globalblue1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] int value)
+        {
+            int result = value + 1;  // Add 1 to the received value
+            return Ok(result);       // Return the result
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ globalblue1/Models/ApiKeyMiddleware.cs
@@ -0,0 +1,39 @@
+﻿namespace globalblue1.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.Configuration;
+    using System.Threading.Tasks;
+
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string ApiKeyHeaderName = "X-API-KEY";
+        private readonly string _apiKey;    
+
+        public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _apiKey = configuration.GetValue<string>("ApiKeySettings:ApiKey");
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("API Key was not provided.");
+                return;
+            }
+
+            if (!_apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401; // Unauthorized
+                await context.Response.WriteAsync("Unauthorized client.");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
